4e3dd41432651259f4f5294c41c166540194a348
JuanXProgramador <juan4cardenas@gmail.com>
2026-05-08 20:00:42 -0500

    feat: implement SyncEngineService to handle background synchronization of local queue tasks to Firestore

--- lib/core/network/sync_engine_service.dart
+++ lib/core/network/sync_engine_service.dart
@@ -2,12 +2,10 @@
 
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 import '../local_storage/database_helper.dart';
 
